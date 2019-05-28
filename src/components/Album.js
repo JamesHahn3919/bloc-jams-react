882011484a0922896f72a89a776f6481df6a9d92
JamesHahn3919 <james.hahn3919@gmail.com>
2019-05-28 04:05:18 -0700

    Audio playback checkpoint checkpoint

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,8 +10,40 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement("audio");
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) {
+        this.setSong(song);
+      }
+      this.play();
+    }
   }
 
   render() {
@@ -37,7 +69,11 @@ class Album extends Component {
           </colgroup>
           <tbody>
             {this.state.album.songs.map((songs, index) => (
-              <tr key={songs.toString()}>
+              <tr
+                className="song"
+                key={index}
+                onClick={() => this.handleSongClick()}
+              >
                 {" "}
                 {songs.number}
                 {index}{" "}
